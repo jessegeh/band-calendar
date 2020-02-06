2b328b061dcbe9af4320893fa098051e1d704a85
Jesse Gehlen <jesse.gehlen@gmail.com>
2020-02-06 16:34:23 +0200

    implement code that pulls in band data from firestore and stores it into the band reducer from band page

--- src/pages/band/band.component.jsx
+++ src/pages/band/band.component.jsx
@@ -25,7 +25,7 @@ class BandPage extends React.Component {
         const { updateCollections } = this.props;
         const collectionRef = firestore.collection('collections');
 
-        this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
+        collectionRef.get().then(snapshot => {
             const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
             updateCollections(collectionsMap);
             this.setState({ loading: false });
