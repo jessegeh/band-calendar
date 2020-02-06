36b1a13bbce10f22bac7f7a952edbb9f673d909c
Jesse Gehlen <jesse.gehlen@gmail.com>
2020-02-06 11:53:42 +0200

    added some mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import './App.css';
+import { GlobalStyle } from './global.styles';
 
 import HomePage from './pages/homepage/homepage.component';
 import BandPage from './pages/band/band.component';
@@ -42,6 +42,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <GlobalStyle />
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
