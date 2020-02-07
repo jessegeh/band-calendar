3495744e4af94023a18e2b6f7a97834dfd5e64bc
Jesse Gehlen <jesse.gehlen@gmail.com>
2020-02-07 15:52:07 +0200

    added containers for collections

--- src/pages/band/band.component.jsx
+++ src/pages/band/band.component.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-// import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
+// import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 
 import { fetchCollectionsStartAsync } from '../../redux/band/band.actions';
-import { selectIsCollectionFetching, selectIsCollectionLoaded } from '../../redux/band/band.selectors';
 
-import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionPageContainer from '../collection/collection.container';
 
-import CollectionPage from '../collection/collection.component';
-
-// const CollectionOverviewWithSpinner = WithSpinner(CollectionsOverview);
-const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
 class BandPage extends React.Component {
     componentDidMount() {
@@ -22,29 +16,25 @@ class BandPage extends React.Component {
     }
 
     render() {
-        const { match, isCollectionFetching, isCollectionLoaded } = this.props;
+        const { match } = this.props;
 
         return (<div className='band-page'>
             {/* <Route exact={`${match.path}`}
                 render={props => (<CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} />
                 )} /> */}
             <Route path={`${match.path}/:collectionId`}
-                render={(props) => (<CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props} />)} />
+                component={CollectionPageContainer}
+                />
         </div>
         );
     }
 }
-
-const mapStateToProps = createStructuredSelector({
-    isCollectionFetching: selectIsCollectionFetching,
-    isCollectionLoaded: selectIsCollectionLoaded
-});
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
   });
 
 export default connect(
-    mapStateToProps,
+    null,
     mapDispatchToProps
 )(BandPage);
