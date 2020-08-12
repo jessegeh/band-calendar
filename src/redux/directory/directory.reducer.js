a42ba8ca5878f8f0835382f94408b9f34f30c9a6
Jesse Gehlen <jesse.gehlen@gmail.com>
2020-08-12 13:10:32 +0300

    photo links fixed

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,32 +1,32 @@
 const INITIAL_STATE = {
     sections: [{
         title: 'the everybody knowns',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/33995556_2046197835593019_5408772660836433920_o.jpg?_nc_cat=111&_nc_sid=dd9801&_nc_ohc=DgyEv3F7ewgAX8i3Wjh&_nc_ht=scontent-hel2-1.xx&oh=f48a9365a5f67f83d89e0a9f8059acd1&oe=5EEAEF4C',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/33995556_2046197835593019_5408772660836433920_o.jpg?_nc_cat=111&_nc_sid=dd9801&_nc_ohc=IKDf07CruhcAX86mkqR&_nc_ht=scontent-hel2-1.xx&oh=a54f74e56fc6dc60928ef411d2c8151b&oe=5F59AD4C',
         id: 1,
         linkUrl: 'band/the%20everybody%20knows'
     },
     {
         title: 'karkki & the car keys',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/54416008_402206860511213_1536307405187448832_n.jpg?_nc_cat=107&_nc_sid=dd9801&_nc_ohc=YZAK9Fg1kr8AX8hduWF&_nc_ht=scontent-hel2-1.xx&oh=98a7b74f585c7a07bb832ba7810b7c04&oe=5EEA6816',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/61029418_434854597246439_4100625274306035712_n.jpg?_nc_cat=110&_nc_sid=8024bb&_nc_ohc=C2infVvh-7QAX9O3nMn&_nc_ht=scontent-hel2-1.xx&oh=7d5cb9163d92daadd6bd5ebe0e7e6fd2&oe=5F58C6D5',
         id: 2,
         linkUrl: 'band/karkki%20&%20the%20car%20keys'
     },
     {
         title: 'pete tnt band',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/s960x960/48269231_2224703417815085_7501314434350972928_o.jpg?_nc_cat=109&_nc_ohc=uFzwwYOwHXAAX84wO_B&_nc_ht=scontent-hel2-1.xx&_nc_tp=1002&oh=93f516f528f238eef31a538fbc05442d&oe=5ECACEC4',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/30704788_420085601783593_8915062959177203712_o.jpg?_nc_cat=106&_nc_sid=dd9801&_nc_ohc=OArmT_eQvtkAX-bukAZ&_nc_ht=scontent-hel2-1.xx&oh=31d55d3b13f8c2a16d118377a47841b7&oe=5F5951A4',
         id: 3,
         linkUrl: 'band/pete%20tnt%20band'
     },
     {
         title: 'teini-pää',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/69203869_691852597995918_9102791976775843840_o.jpg?_nc_cat=111&_nc_sid=dd9801&_nc_ohc=35D6-oFFgmAAX-4qfTt&_nc_ht=scontent-hel2-1.xx&oh=8a3d19ef4e9e00064eb04c3a7d0626eb&oe=5EE95EF5',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/69203869_691852597995918_9102791976775843840_o.jpg?_nc_cat=111&_nc_sid=dd9801&_nc_ohc=WTgzrvJwt0YAX_lk2Jb&_nc_ht=scontent-hel2-1.xx&oh=7b60109359d6dee04b626298a2b57650&oe=5F581CF5',
         size: 'big',
         id: 4,
         linkUrl: 'band/teini-p%C3%A4%C3%A4'
     },
     {
         title: 'heyday',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/69220754_104013347643084_2429825344816021504_o.jpg?_nc_cat=100&_nc_ohc=W6D7KcflkD0AX9W7Ubu&_nc_ht=scontent-hel2-1.xx&oh=5ee6d74a65ac80f74f91683a10a96b87&oe=5EC950BC',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t1.0-9/69220754_104013347643084_2429825344816021504_o.jpg?_nc_cat=100&_nc_sid=dd9801&_nc_ohc=VCwGCIi6Ix4AX-y_e1v&_nc_ht=scontent-hel2-1.xx&oh=14164b9c69a4492c9e6e4317e730ee60&oe=5F59D4C4',
         size: 'big',
         id: 5,
         linkUrl: 'band/heyday'
@@ -45,7 +45,7 @@ const INITIAL_STATE = {
     },
     {
         title: 'Pink Rat',
-        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t31.0-8/s960x960/18278971_10211700763187198_5476127454371900219_o.jpg?_nc_cat=110&_nc_sid=b386c4&_nc_ohc=XcwbkPHHJqoAX8WsdOU&_nc_ht=scontent-hel2-1.xx&_nc_tp=7&oh=a8dd26d15f4176a3bcbe75c70e989a41&oe=5EEAA060',
+        imageUrl: 'https://scontent-hel2-1.xx.fbcdn.net/v/t31.0-8/s960x960/18278971_10211700763187198_5476127454371900219_o.jpg?_nc_cat=110&_nc_sid=b386c4&_nc_ohc=3tflfNGcq0QAX8hmKBh&_nc_ht=scontent-hel2-1.xx&_nc_tp=7&oh=85764fe55e82cbdbddf4eefb9ec332ce&oe=5F595E60',
         id: 8,
         linkUrl: 'band/pink%20rat'
     }
